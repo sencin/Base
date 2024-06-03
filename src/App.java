888234d682e5cfb70503fc7e8d81ffce9212bc0a
xyzna <null>
2024-06-03 19:48:47 +0800

    added filtering for usertype

--- src/App.java
+++ src/App.java
@@ -1,12 +1,13 @@
 import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         try {
         System.out.print("Enter Decimal Number: ");
         Scanner read = new Scanner(System.in);
-        int decimal = Math.abs(read.nextInt());
         conversion instance = new conversion();
-
+        int decimal = Integer.parseInt(instance.filterInput(read.nextLine()));
+        System.out.printf("Input Cleaned: %d%n",decimal);
         System.out.println("====RESULTS====");
         instance.binary(decimal);
         instance.octal_Hexa_conversion(decimal, 8);
@@ -14,18 +15,17 @@ public class App {
         System.out.println("===============");
         
         } catch (Exception e) {
-           System.out.printf("Enter a valid integer number %n%s",e.fillInStackTrace());
+           System.out.printf("Error: %s",e.fillInStackTrace());
         }
     }
 }
 
 class conversion{
-   
    public void binary(int decimal){
-    int zero =0;
+    int zero = 0;
     String output = "";
     int binary = 2;
-
+    
     if(decimal ==0){
         System.out.println("BINARY: 0");
         return;
@@ -36,7 +36,6 @@ class conversion{
         }
           System.out.printf("BINARY: %s %n",new StringBuilder(output).reverse());
     }
-    
     public void octal_Hexa_conversion(double decimal,int base){
         String hexadecimal ="0123456789ABCDEF";
         List<String> output = new ArrayList<>();
@@ -47,13 +46,11 @@ class conversion{
         if(base !=octal && base!=hexa ){
             System.out.println("Base 8 and 16 is Allowed.");
             return;
-        }
-        
+        } 
         if(decimal == 0){
             System.out.println((base==hexa) ? "HEXA: 0":"OCTAL: 0");
             return;
         }
-
         while(decimal>0){
             decimal/=base;
             String ternary = (decimal%base == 0)? "0":returnremainder(decimal,base);
@@ -70,6 +67,15 @@ class conversion{
                 }       
         }
         System.out.printf((base==hexa) ? "HEXA: %s %n":"OCTAL: %s %n", new StringBuilder(processedString).reverse());
+    }
+    public String filterInput (String input){
+        String filtered = "";
+        for(char c : input.toCharArray()){
+            if(Character.isDigit(c)){
+                filtered+=c;
+            }
+        }
+        return filtered.toString();
     }
     private String returnremainder(double decimal, int base){
         double remainder = base *(decimal -  Math.floor(decimal));
