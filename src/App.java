74e5d564819771f6044db4bd15abf36f9938fb71
xyzna <null>
2024-06-06 15:58:14 +0800

    changed datatype names to a more readable name

--- src/App.java
+++ src/App.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         try {
-        System.out.print("Enter Decimal Number: ");
         Scanner read = new Scanner(System.in);
         conversion instance = new conversion();
+        System.out.print("Enter Decimal Number: ");
         double decimal = Double.parseDouble(instance.filterInput(read.nextLine()));
-        System.out.printf("Data Cleaned: %.2f %n",decimal);
+        System.out.printf("Data Cleaned: %.1f %n",decimal);
         System.out.println("===BINARY===");
         instance.convert(decimal, 2);
         System.out.println("====OCTAL===");
@@ -16,9 +16,8 @@ public class App {
         instance.convert(decimal, 16);
         System.out.println("============");
         } catch (Exception e) {
-           System.out.printf("Error: %s",e.fillInStackTrace());
+        System.out.printf("Error: %s",e.fillInStackTrace());
         }
-       
     }
 }
 
@@ -36,7 +35,6 @@ class conversion{
         double remainder = base *(decimal -  Math.floor(decimal));
         return String.format("%.0f",Math.floor(remainder));
     }
-
     private char getHexadecimalvalues(int index){
         String hexadecimal ="0123456789ABCDEF";
         return hexadecimal.charAt(index);
@@ -45,10 +43,12 @@ class conversion{
     public void convert(double decimal, int base){
        
         List<String> integercontainer = new ArrayList<>();
-        String processedString ="";
-        List<String> processedDecimal = new ArrayList<>();
-        double fractionpart = 0.0;
-        StringBuilder decimals = new StringBuilder();
+        String processedInteger ="";
+
+        double floatingPointContainer = 0.0;
+        List<String> decimalContainer = new ArrayList<>();
+        StringBuilder processedDecimal = new StringBuilder();
+        
         int hexa = 16;
         int octal = 8;
         int binary =2;
@@ -60,7 +60,7 @@ class conversion{
 
         if(decimal - Math.floor(decimal) != 0){
             String[] split = String.valueOf(decimal).split("\\.");
-            fractionpart = decimal - Integer.parseInt(split[0]);
+            floatingPointContainer = decimal - Integer.parseInt(split[0]);
             decimal = Integer.parseInt(split[0]);
         }
         while(decimal>0){
@@ -70,14 +70,13 @@ class conversion{
             decimal = (decimal<1)? 0:decimal;
         }
 
-        if(fractionpart != 0.0){
-          boolean level = true;
-            while (level) {
-            double value = fractionpart*base; 
-            processedDecimal.add(String.format("%.0f",Math.floor(value)));
-            
-            fractionpart = 0 + value % 1;
-               if(processedDecimal.size()==7){
+        if(floatingPointContainer != 0.0){
+          boolean isTrue = true;
+            while (isTrue) {
+            double value = floatingPointContainer*base; 
+            decimalContainer.add(String.format("%.0f",Math.floor(value)));
+            floatingPointContainer = 0 + value % 1;
+               if(decimalContainer.size()==7){
                 break;
                }
             }
@@ -86,27 +85,27 @@ class conversion{
         for(int a =0;a<integercontainer.size();a++){
             if(base==hexa){
                 int num = Integer.parseInt(integercontainer.get(a));
-                processedString+=getHexadecimalvalues(num);
-                if(!processedDecimal.isEmpty()){
-                    for(int z =0;z<processedDecimal.size();z++){
-                        int num2 = Integer.parseInt(processedDecimal.get(z));
-                        decimals.append(getHexadecimalvalues(num2));
-                    }                    
-                }
+                processedInteger+=getHexadecimalvalues(num);     
+                    if(!decimalContainer.isEmpty()){
+                        for(int z =0;z<decimalContainer.size();z++){
+                            int num2 = Integer.parseInt(decimalContainer.get(z));
+                            processedDecimal.append(getHexadecimalvalues(num2));
+                        }                    
+                    }
             }
             else{
-               processedString+= integercontainer.get(a).toString();
+                processedInteger+= integercontainer.get(a).toString();
             }       
         }
-  
-       StringBuilder reversedProcessedString = new StringBuilder(processedString).reverse();    
+   
+       StringBuilder reversedProcessedString = new StringBuilder(processedInteger).reverse();    
        
-        if(!processedDecimal.isEmpty()){
-            for (String string : processedDecimal) {
-                decimals.append(string);
+        if(!decimalContainer.isEmpty()){
+            for (String string : decimalContainer){
+                processedDecimal.append(string);
             } 
-            String filteredtext = decimals.toString().substring(0,decimals.length()/2);
-            System.out.printf("OUTPUT: %s.%s \n",reversedProcessedString.toString(),filteredtext);       
+            String decimalformat = processedDecimal.substring(0,processedDecimal.length()/2);
+            System.out.printf("OUTPUT: %s.%s \n",reversedProcessedString.toString(),decimalformat.toString());       
         }
         else{
             System.out.printf("OUTPUT: %s \n", reversedProcessedString);
